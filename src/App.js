6312040d83b64be6d4e04f9a207190b3521317d9
david.khowanto <david.khowanto@caditglobal.com>
2024-04-12 15:47:58 +0700

    fix: bank account moved to component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@
  * TODO: 
   1. ID/EN [IN PROGRESS]
   2. if "to" parameter not exists, Dear will show family and friends [DONE]
-  3. Bandung Wedding Dinner RSVP [IN PROGRESS]
+  3. Bandung Wedding Dinner RSVP [DONE]
  */
 import BandungRSVP from './bandung-rsvp.js';
-
+import BankAcc from "./back-acc.js";
 import './App.css';
 import { React, useState, useRef } from 'react';
-import { FaClone, FaExternalLinkAlt, FaVolumeMute, FaVolumeUp } from 'react-icons/fa';
-
-import { toast, ToastContainer } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
-
+import { FaVolumeMute, FaVolumeUp } from 'react-icons/fa';
 
 function App() {
   const queryParam = new URLSearchParams(window.location.search);
@@ -20,6 +16,7 @@ function App() {
   const isRsvp = queryParam.get("rsvp") === "true"; // should be true to show bandung rsvp
 
   const [audioStatus, changeAudioStatus] = useState(true);
+
   const myRef = useRef();
 
   const musicHandling = () => {
@@ -29,15 +26,17 @@ function App() {
   };
 
   return (
+
     <div className="flex flex-col overflow-hidden h-max">
+
       <audio autoPlay ref={myRef} src='song.mp3' />
 
       <div className='fixed bottom-0 z-10'>
         <button onClick={musicHandling} className='p-3 m-2 w-fit bg-gray-400 rounded-full'>
           {audioStatus ?
-          <FaVolumeUp color='white' /> :
-          <FaVolumeMute color='white' />
-        }
+            <FaVolumeUp color='white' /> :
+            <FaVolumeMute color='white' />
+          }
         </button>
         <button className='rounded-full bg-gray-400 w-10 h-10 text-slate-200'>
           {true ?
@@ -80,7 +79,6 @@ function App() {
       <div className="flex flex-col mx-auto w-full justify-items-center text-center fade-in -z-1">
         {/* GROOM, BRIDE, FAMILY NAMES */}
         <div className="grid gap-8 w-full pb-12 border-8 border-white drop-shadow-lg shadow-inner bg-names">
-          {/* <div className="pt-5 mb-6 grid gap-3 divide-y-2 divide-[#999090] text-[#999090] top-font text-lg font-medium"> */}
           <div className="mt-5 mb-6 p-2 grid gap-3 divide-y-2 bg-white bg-opacity-70 divide-[#999090] text-[#999090] top-font text-lg font-medium mx-auto rounded-xl">
             <div >Dear,</div>
             <div className="w-fit pt-2 text-2xl mx-auto">{name ? name : "Family and Friends"}</div>
@@ -93,17 +91,17 @@ function App() {
           </div>
 
           <div className="flex flex-col leading-10 gap-4">
-            <div className="name-font text-[2.2rem] sm:text-[2.6rem]">David Khowanto Lim</div>
+            <div className="name-font text-[2.2rem] sm:text-[2.6rem]">David Khowanto Lim (David)</div>
             {/* <div className="text-lg capitalize parent-font leading-5">first son of<br />lim yam hiong & khow phek huan</div> */}
             <div className="text-lg capitalize parent-font leading-5">first son of</div>
             <div className='flex justify-center gap-4 w-full mx-auto max-w-3xl'>
               <div className='w-1/2'>
                 <div className="text-lg capitalize parent-font leading-7">lim yam hiong</div>
-                <div className="text-md capitalize parent-font leading-7">"kok liang"</div>
+                <div className="text-md capitalize parent-font leading-7">(kok liang)</div>
               </div>
               <div className='w-1/2'>
-                <div className="text-lg capitalize parent-font leading-7">ho phek huan</div>
-                <div className="text-md capitalize parent-font leading-7">"khow phek luan"</div>
+                <div className="text-lg capitalize parent-font leading-7">Khow phek luan</div>
+                <div className="text-md capitalize parent-font leading-7">(phek huan)</div>
               </div>
             </div>
           </div>
@@ -111,17 +109,17 @@ function App() {
           <div className="name-font text-[2rem] sm:text-[2.6rem] m-3">&</div>
 
           <div className="flex flex-col leading-10 gap-4">
-            <div className="name-font text-[2.2rem] sm:text-[2.6rem]">Eleonora Julianti Mardi Utami</div>
+            <div className="name-font text-[2.2rem] sm:text-[2.6rem]">Eleonora Julianti Mardi Utami (Ellen)</div>
             {/* <div className="text-lg capitalize parent-font leading-5">first Daughter of<br />apolonius sumardijono & elite julili</div> */}
             <div className="text-lg capitalize parent-font leading-5">first daughter of</div>
             <div className='flex justify-center gap-4 w-full mx-auto max-w-3xl'>
               <div className='w-1/2'>
                 <div className="text-lg capitalize parent-font leading-7">apolonius sumardijono</div>
-                <div className="text-md capitalize parent-font leading-7">"apolonius"</div>
+                <div className="text-md capitalize parent-font leading-7">(Apolo)</div>
               </div>
               <div className='w-1/2'>
                 <div className="text-lg capitalize parent-font leading-7">elite julili</div>
-                <div className="text-md capitalize parent-font leading-7">"elite"</div>
+                <div className="text-md capitalize parent-font leading-7">(elite)</div>
               </div>
             </div>
           </div>
@@ -136,7 +134,7 @@ function App() {
             <div>14.30 WIB</div>
             <a className="underline underline-offset-2 text-xl" target="_blank" rel="noopener noreferrer" href="https://maps.app.goo.gl/K7KnnNnHV1f9zGnY7" >Saint Peter Cathedral</a>
             <div className="mx-auto">
-              Jl. Merdeka, no. 14, Bandung
+              Jl. Merdeka, no. 14, <br /> Kota Bandung
             </div>
             <a className='bg-[#999090] px-5 py-1 text-sm text-white rounded-sm z-20' target="_blank" rel="noopener noreferrer" href="https://maps.app.goo.gl/K7KnnNnHV1f9zGnY7">get direction</a>
           </div>
@@ -145,11 +143,11 @@ function App() {
             <div className="font-extrabold">reception</div>
             <div>Saturday, 25th May 2024</div>
             <div>17.00 - 20.00 WIB</div>
-            <a className="underline underline-offset-2 text-xl" target="_blank" rel="noopener noreferrer" href="https://maps.app.goo.gl/8Rj8MCY5tRYaJxum8" >Star Restaurant </a>
+            <a className="underline underline-offset-2 text-xl" target="_blank" rel="noopener noreferrer" href="https://maps.app.goo.gl/8Rj8MCY5tRYaJxum8" >Star Restaurant <br /> Function Hall lt. 3</a>
             <br />
-            <a className="text-lg" target="_blank" rel="noopener noreferrer" href="https://maps.app.goo.gl/8Rj8MCY5tRYaJxum8">function hall lt. 3</a>
+            <a className="text-lg" target="_blank" rel="noopener noreferrer" href="https://maps.app.goo.gl/8Rj8MCY5tRYaJxum8"></a>
             <div className="mx-auto">
-              Jl. Gajah Mada No. 189, Pontianak
+              Jl. Gajah Mada No. 189,  <br />Kota Pontianak
             </div>
 
             <a className='bg-[#999090] px-5 py-1 text-sm text-white rounded-sm z-20' href={"https://maps.app.goo.gl/8Rj8MCY5tRYaJxum8"} target='_blank' rel="noopener noreferrer">get direction</a>
@@ -166,20 +164,11 @@ function App() {
             <div className="content-font text-lg font-medium tracking-wider leading-9">
               <div className="font-extrabold">wedding gifts</div>
               <div className="max-w-[25rem] mx-auto">
-                Whilst it's enough of your present and prayer, it would be our pleasure to receive your special gift of love
+                {/* Whilst it's enough of your present and prayer, it would be our pleasure to receive your special gift of love */}
+                Your presence and prayers are truly priceless. Your heartfelt gift of love would add immeasurable joy to our day.
               </div>
-              <div className="grid p-2 grid-flow-col auto-cols-max text-left mt-2 bg-white w-fit mx-auto rounded-lg drop-shadow-md">
-                <div className="w-52" onClick={copyToClipboard} data-num="222222">
-                  <div>BCA XXXXXXXX</div>
-                  <div>a.n. David Ellen</div>
-                </div>
-                <FaClone className="h-12 w-4" onClick={copyToClipboard} />
-              </div>
-            </div>
-            <div>
-              <ToastContainer
-                style={{ width: "140px" }}
-              />
+              <BankAcc name="david_bca" ></BankAcc>
+              <BankAcc name="ellen_bca"></BankAcc>
             </div>
           </div>
 
@@ -207,17 +196,5 @@ function App() {
   );
 }
 
-const copyToClipboard = (e) => {
-  console.log(e);
-  navigator.clipboard.writeText("12345678");
-  toast.success("Copied", {
-    width: 100,
-    autoClose: 2000,
-    position: "bottom-center",
-    autoClose: 3000,
-    hideProgressBar: true,
-    pauseOnFocusLoss: false,
-  });
-};
 
 export default App;
---
+++ src/back-acc.js
@@ -0,0 +1,31 @@
+
+import { FaClone, FaClipboardCheck } from 'react-icons/fa';
+import { useState } from "react";
+
+const BANK_ACC = {
+  david_bca: { no: "7155229608", name: "David Khowanto", bank: "BCA" },
+  ellen_bca: { no: "0083580833", name: "Eleonora Julianti Mardi", bank: "BCA" }
+};
+
+export default function BankAcc(props) {
+  const [isBankAccCopied, setIsBankAccCopied] = useState(false);
+  const info = BANK_ACC[props.name];
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(info.no);
+    setIsBankAccCopied(true);
+    setTimeout(() => {
+      setIsBankAccCopied(false);
+    }, 2000);
+  };
+
+  return (<div className="grid p-2 grid-flow-col auto-cols-max text-left mt-2 bg-white w-fit mx-auto rounded-lg drop-shadow-md" onClick={copyToClipboard}>
+    <div className="w-52" >
+      <div>{info.bank} {info.no}</div>
+      <div>{info.name}</div>
+    </div>
+    {isBankAccCopied ? <FaClipboardCheck className="h-12 w-4 text-green-500 transition-transform duration-300" /> : <FaClone className="h-12 w-4" />}
+  </div>
+  );
+}
+
