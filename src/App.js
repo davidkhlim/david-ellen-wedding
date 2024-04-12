dec43aabe5b457dde86381292c13b76ecdd06e95
david.khowanto <david.khowanto@caditglobal.com>
2024-04-12 10:36:51 +0700

    Feat: RSVP Form with Supabase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 /**
  * TODO: 
-  1. ID/EN
+  1. ID/EN [IN PROGRESS]
   2. if "to" parameter not exists, Dear will show family and friends [DONE]
-  3. Bandung Wedding Dinner RSVP / no
-  4. coba RSVP pakai google form
-  5. ganti kata2
+  3. Bandung Wedding Dinner RSVP [IN PROGRESS]
  */
+import BandungRSVP from './bandung-rsvp.js';
 
 import './App.css';
 import { React, useState, useRef } from 'react';
@@ -14,49 +13,40 @@ import { FaClone, FaExternalLinkAlt, FaVolumeMute, FaVolumeUp } from 'react-icon
 import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 
-import { createClient } from "@supabase/supabase-js";
-
-async function SubmitForm(name, pax) {
-  const supabase = createClient("https://udwruewtbwimrrlfekig.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkd3J1ZXd0YndpbXJybGZla2lnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY0NjY4ODQsImV4cCI6MjAyMjA0Mjg4NH0.bd5izwVQucwICbx5cj_we0MnKQYvCR4fB8Ypts-_2JI");
-
-  const { data } = await supabase.from("wedding-dinner-rsvp").insert({ name: name, pax: 1 });
-  console.log(data);
-}
 
 function App() {
   const queryParam = new URLSearchParams(window.location.search);
-  const name = queryParam.get("to");
+  const name = queryParam.get("name");
+  const isRsvp = queryParam.get("rsvp") === "true"; // should be true to show bandung rsvp
+
   const [audioStatus, changeAudioStatus] = useState(true);
   const myRef = useRef();
 
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText("12345678");
-    toast.success("Copied to clipboard", {
-      width: 100,
-      autoClose: 2000,
-      position: "bottom-center",
-      autoClose: 3000,
-      hideProgressBar: true,
-      pauseOnFocusLoss: false,
-    });
-  };
-
   const musicHandling = () => {
-    if(audioStatus == true) myRef.current.pause();
+    if (audioStatus == true) myRef.current.pause();
     else myRef.current.play();
     changeAudioStatus(!audioStatus);
-  }
+  };
 
   return (
     <div className="flex flex-col overflow-hidden">
-      <audio autoPlay ref={myRef} src='song.mp3'/>
-          
-      <button onClick={musicHandling} className='p-3 m-2 w-fit bg-gray-400 rounded-full fixed bottom-0 z-10'>
-        { audioStatus ? 
+      <audio autoPlay ref={myRef} src='song.mp3' />
+
+      <div className='fixed bottom-0  z-10'>
+        <button onClick={musicHandling} className='p-3 m-2 w-fit bg-gray-400 rounded-full'>
+          {audioStatus ?
           <FaVolumeUp color='white' /> :
           <FaVolumeMute color='white' />
         }
-      </button> 
+        </button>
+        <button className='rounded-full bg-gray-400 w-10 h-10 text-slate-200'>
+          {true ?
+            "ID" :
+            "EN"
+          }
+        </button>
+      </div>
+
 
       {/* OPENING IMAGE */}
       <div className="min-h-screen fade-in relative w-full">
@@ -74,7 +64,7 @@ function App() {
       </div>
 
       {/* CONTENT */}
-      <div className="grid gap-6 mx-auto w-full justify-items-center text-center pb-6 schedule fade-in -z-1">
+      <div className="flex flex-col mx-auto w-full justify-items-center text-center pb-6 fade-in -z-1">
 
         {/* GROOM, BRIDE, FAMILY NAMES */}
         <div className="grid gap-8 w-full pb-12 border-b-8 border-t-8 border-white drop-shadow-lg shadow-inner bg-names">
@@ -103,14 +93,12 @@ function App() {
         </div>
 
         {/* SCHEDULE */}
-        <div className='box-border px-8 max-w-screen w-screen'>
+        <div className='box-border px-8 schedule sm:bg-cover bg-contain max-w-screen w-screen'>
+
           <div className="mt-12 content-font text-base sm:text-lg font-medium tracking-wider leading-9">
             <div className="font-extrabold">holy matrimony</div>
             <div>Saturday, 18th May 2024 | xx.xx WIB</div>
             <a className="underline underline-offset-2 text-xl" target="_blank" rel="noopener noreferrer" href="https://maps.app.goo.gl/K7KnnNnHV1f9zGnY7" >Saint Peter Cathedral</a>
-              {/* <a className="underline underline-offset-2 text-xl" target="_blank" rel="noopener noreferrer" href="https://maps.app.goo.gl/K7KnnNnHV1f9zGnY7" >
-                <FaExternalLinkAlt className="inline-block ml-2 mb-1 h-3 w-4" />
-              </a> */}
             <div className="mx-auto">
               Jl. Merdeka, no. 14, Bandung
             </div>
@@ -121,9 +109,6 @@ function App() {
             <div className="font-extrabold">reception</div>
             <div>Saturday, 25th May 2024 | xx.xx WIB</div>
             <a className="underline underline-offset-2 text-xl" target="_blank" rel="noopener noreferrer" href="https://maps.app.goo.gl/8Rj8MCY5tRYaJxum8" >Star Restaurant </a>
-              {/* <a className="" target="_blank" rel="noopener noreferrer" href="https://maps.app.goo.gl/8Rj8MCY5tRYaJxum8" >
-                <FaExternalLinkAlt className="ml-2 mb-1 h-3 w-4 inline-block" />
-              </a> */}
             <br />
             <a className="text-lg" target="_blank" rel="noopener noreferrer" href="https://maps.app.goo.gl/8Rj8MCY5tRYaJxum8">function hall lt. 3</a>
             <div className="mx-auto">
@@ -133,22 +118,21 @@ function App() {
             <a className='bg-[#999090] px-5 py-1 text-sm text-white rounded-sm z-20' href={"https://maps.app.goo.gl/8Rj8MCY5tRYaJxum8"} target='_blank' rel="noopener noreferrer">get direction</a>
           </div>
 
-          <div className="mt-12 pb-6 content-font text-lg font-medium tracking-wider leading-9">
-            <div className="font-extrabold">reception rsvp</div>
-            <div>please let us know if you'll join us</div>
-            <div className="pb-2 drop-shadow-lg">
-              <iframe className="w-[350px] mx-auto h-[350px] rounded-lg" src="https://tally.so/r/woGZWe" title="" />
-              {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfZ5fXBMmFDF6qHHX7szGJFwRC5XqZAwc9Yq3X8U1DGYQqV9A/viewform?embedded=true" width="640" height="1116" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> */}
-            </div>
-          </div>
+          {/* RSVP */}
+          {isRsvp ? <>
+            <BandungRSVP></BandungRSVP></>
+            : null
+          }
 
           {/* WEDDING GIFTS */}
           <div className="w-full mx-auto">
             <div className="mt-6 mb-2 content-font text-lg font-medium tracking-wider leading-9">
               <div className="font-extrabold">wedding gifts</div>
-              <div className="max-w-[25rem] mx-auto">Whilst it’s enough of your present and prayer, it would be our pleasure to receive your special gift of love</div>
+              <div className="max-w-[25rem] mx-auto">
+                Whilst it’s enough of your present and prayer, it would be our pleasure to receive your special gift of love
+              </div>
               <div className="grid p-2 grid-flow-col auto-cols-max text-left mt-2 bg-white w-fit mx-auto rounded-lg drop-shadow-md">
-                <div className="w-52" onClick={copyToClipboard}>
+                <div className="w-52" onClick={copyToClipboard} data-num="222222">
                   <div>BCA XXXXXXXX</div>
                   <div>a.n. David Ellen</div>
                 </div>
@@ -156,8 +140,8 @@ function App() {
               </div>
             </div>
             <div>
-              <ToastContainer 
-                style={{width: "230px"}}
+              <ToastContainer
+                style={{ width: "140px" }}
               />
             </div>
 
@@ -186,5 +170,18 @@ function App() {
     </div>
   );
 }
+
+const copyToClipboard = (e) => {
+  console.log(e);
+  navigator.clipboard.writeText("12345678");
+  toast.success("Copied", {
+    width: 100,
+    autoClose: 2000,
+    position: "bottom-center",
+    autoClose: 3000,
+    hideProgressBar: true,
+    pauseOnFocusLoss: false,
+  });
+};
 
 export default App;
---
+++ src/bandung-rsvp.js
@@ -0,0 +1,41 @@
+import { createClient } from "@supabase/supabase-js";
+import { useState } from "react";
+
+
+function BandungRSVP() {
+  const [name, setName] = useState("");
+  const [pax, setPax] = useState("");
+
+  async function SubmitForm() {
+    const supabase = createClient("https://udwruewtbwimrrlfekig.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkd3J1ZXd0YndpbXJybGZla2lnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY0NjY4ODQsImV4cCI6MjAyMjA0Mjg4NH0.bd5izwVQucwICbx5cj_we0MnKQYvCR4fB8Ypts-_2JI");
+
+    const { data } = await supabase.from("wedding-dinner-rsvp").insert({ name: name, pax: pax });
+    console.log(data);
+    setName("");
+    setPax("");
+  };
+
+  return (
+    <div className="mt-12 pb-6 content-font text-lg font-medium tracking-wider leading-9">
+      <div className="font-extrabold">reception rsvp</div>
+      <div>please let us know if you'll join us</div>
+
+      <div className="bg-white mx-auto w-max flex flex-col gap-8 p-8 rounded-lg shadow">
+        <input placeholder="full name" type="text" className="border-2 border-gray-400 px-3 py-2 rounded-lg" value={name} onChange={(e) => setName(e.target.value)}></input>
+        <div className="flex justify-between items-center">
+          <input type="number" min='1' max='10' className="border-2 border-gray-400 rounded-lg px-3 py-2" placeholder="1" value={pax} onChange={(e) => setPax(e.target.value)}></input>
+          <label>number of guests</label>
+        </div>
+        <button className="bg-[#999090] p-2 rounded-full text-white" onClick={SubmitForm}>I'll be there</button>
+      </div>
+
+
+      {/* <div className="pb-2 drop-shadow-lg">
+        <iframe className="w-[350px] mx-auto h-[350px] rounded-lg" src="https://tally.so/r/woGZWe" title="" /> */}
+      {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfZ5fXBMmFDF6qHHX7szGJFwRC5XqZAwc9Yq3X8U1DGYQqV9A/viewform?embedded=true" width="640" height="1116" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> */}
+      {/* </div> */}
+    </div>
+  );
+}
+
+export default BandungRSVP;
