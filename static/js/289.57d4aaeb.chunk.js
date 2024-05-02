d4c24c3d35a16a6ff0d8fe7777af45baccf16e5f
david.khowanto <david.khowanto@caditglobal.com>
2024-05-02 10:42:50 +0700

    Deploy React app to GitHub Pages

---
+++ static/js/289.57d4aaeb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwedding_invitation=self.webpackChunkwedding_invitation||[]).push([[289],{3289:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.57d4aaeb.chunk.js.map
